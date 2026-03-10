bf8a270a3e366c9892fca17b143ae08ce42e216a
Lunoed <meispandaren@gmail.com>
2026-03-10 23:14:56 +0300

    добавил в функционал поиск файлов по названию и чтение файлов .json

--- gendif/programm/progr_body.py
+++ gendif/programm/progr_body.py
@@ -1,5 +1,36 @@
 import argparse
+import json
+import os
+#import orjson 
 
+def show_info(data:dict):
+    info = json.dumps(data, indent=4)
+    print(info)
+
+def find_file(file_name:str):
+    for path, dir_names, files in os.walk("/home"):
+        for file in files:
+            if file == file_name:
+                path = os.path.join(path, file)
+                print(path)
+                print(f'type(path) = {type(path)=}')
+
+def find_path(file_name:str) ->str:
+    for root, _, files in os.walk("/home"):
+        for file in files:
+            if file == file_name:
+                path_to_file = os.path.join(root, file_name)
+    return path_to_file
+
+def read_file(file_name:str):
+    path_to_file = find_path(file_name)
+    with open(path_to_file, "r", encoding='utf-8') as f:
+        data = json.load(f)
+        show_info(data)
+    return data
+
+def work_with_file():
+    ...
 
 def run_gendif():
     parser = argparse.ArgumentParser(
@@ -12,3 +43,7 @@ def run_gendif():
                         help='set format of output')
     args = parser.parse_args()
     print(args)
+
+file_name = 'file2.json'
+#read_file(file_name)
+read_file(file_name)
