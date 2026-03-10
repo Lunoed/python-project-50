e227c3d8c7394fcb7a20c5eaaee81c1620152da3
Lunoed <meispandaren@gmail.com>
2026-03-10 21:03:40 +0300

    сделал второй шаг проекта - добавил опцию -f --format

--- gendif/programm/progr_body.py
+++ gendif/programm/progr_body.py
@@ -8,5 +8,7 @@ def run_gendif():
     )
     parser.add_argument("first_file")
     parser.add_argument("second_file")
+    parser.add_argument('-f', '--format',
+                        help='set format of output')
     args = parser.parse_args()
     print(args)
