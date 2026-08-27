5862696be3c389fe8277b06a8df64d6ba9f282af
Lunoed <meispandaren@gmail.com>
2026-08-27 10:14:37 +0300

    создал модуль gendiff,начальные файлы программы (gendiff.py и элементарный код запуска) а также настроил временную точку входа

---
+++ gendiff/scripts/gendiff.py
@@ -0,0 +1,18 @@
+import argparse
+
+
+def make_parser():
+    parser = argparse.ArgumentParser(
+        prog = 'gendif',
+        description='Compares two configuration files and shows a difference.'
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    args = parser.parse_args()
+
+def main():
+    make_parser()
+
+
+if __name__ == '__main__':
+    main()
